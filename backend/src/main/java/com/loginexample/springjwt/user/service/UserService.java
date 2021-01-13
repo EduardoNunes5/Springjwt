5bcfdb7ac9ae0316deac44ccbaaa6269cabaa681
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-01-12 22:09:12 -0300

    create user service

--- backend/src/main/java/com/loginexample/springjwt/user/service/UserService.java
+++ backend/src/main/java/com/loginexample/springjwt/user/service/UserService.java
@@ -1,2 +1,45 @@
-package com.loginexample.springjwt.user.service;public class UserService {
+package com.loginexample.springjwt.user.service;
+
+import com.loginexample.springjwt.user.dto.UserDTO;
+import com.loginexample.springjwt.user.dto.UserMessageDTO;
+import com.loginexample.springjwt.user.entity.User;
+import com.loginexample.springjwt.user.mapper.UserMapper;
+import com.loginexample.springjwt.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    private UserMapper userMapper = UserMapper.INSTANCE;
+
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserMessageDTO create(UserDTO userDTO){
+        User userToBeSaved = userMapper.toModel(userDTO);
+        encodePassword(userToBeSaved);
+        userRepository.save(userToBeSaved);
+        return new UserMessageDTO("User created");
+    }
+
+    public List<UserDTO> findAll(){
+        return this.userRepository.findAll()
+                .stream()
+                .map(userMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    private void encodePassword(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+    }
 }
