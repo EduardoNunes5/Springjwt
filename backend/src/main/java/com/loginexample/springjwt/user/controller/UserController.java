f894ab3a7e74f5b2a57ea541b8424aeeb8b3b028
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-01-12 22:09:39 -0300

    user creation

--- backend/src/main/java/com/loginexample/springjwt/user/controller/UserController.java
+++ backend/src/main/java/com/loginexample/springjwt/user/controller/UserController.java
@@ -1,2 +1,32 @@
-package com.loginexample.springjwt.user.controller;public class UserController {
+package com.loginexample.springjwt.user.controller;
+
+import com.loginexample.springjwt.user.dto.UserDTO;
+import com.loginexample.springjwt.user.dto.UserMessageDTO;
+import com.loginexample.springjwt.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserMessageDTO create(@RequestBody UserDTO userDTO){
+        return userService.create(userDTO);
+    }
+
+
+    @GetMapping
+    public List<UserDTO> findAll(){
+        return userService.findAll();
+    }
 }
