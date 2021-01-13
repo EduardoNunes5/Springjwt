cc8dd9c71c3c150eea668610df352de90bc6faec
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-01-12 22:08:50 -0300

    user repository

--- backend/src/main/java/com/loginexample/springjwt/user/repository/UserRepository.java
+++ backend/src/main/java/com/loginexample/springjwt/user/repository/UserRepository.java
@@ -1,2 +1,13 @@
-package com.loginexample.springjwt.user.repository;public interface UserRepository {
+package com.loginexample.springjwt.user.repository;
+
+import com.loginexample.springjwt.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
